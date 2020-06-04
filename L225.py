fd622c2e4b2c66eadafa5da8e5b4d43f2971c178
zy-zxc <827976530@qq.com>
2020-06-04 13:21:55 +0800

    反转二叉树--迭代

--- L225.py
+++ L225.py
@@ -1,56 +1,20 @@
-#队列实现栈
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
 
-class MyStack(object):
-
-    def __init__(self):
+class Solution(object):
+    def invertTree(self, root):
         """
-        Initialize your data structure here.
+        :type root: TreeNode
+        :rtype: TreeNode
         """
-        self.q = []
-
-    def push(self, x):
-        """
-        Push element x onto stack.
-        :type x: int
-        :rtype: None
-        """
-        self.q.append(x)
-        self.t = x
-
-    def pop(self):
-        """
-        Removes the element on top of the stack and returns that element.
-        :rtype: int
-        """
-        p = []
-        for i in range(len(self.q) - 1):
-            self.t = self.q.pop(0)
-            p.append(self.t)
-
-        x = self.q.pop()
-        self.q = p
-        return x
-
-    def top(self):
-        """
-        Get the top element.
-        :rtype: int
-        """
-        return self.t
-
-    def empty(self):
-        """
-        Returns whether the stack is empty.
-        :rtype: bool
-        """
-        if len(self.q) != 0:
-            return False
-        else:
-            return True
-
-# Your MyStack object will be instantiated and called as such:
-# obj = MyStack()
-# obj.push(x)
-# param_2 = obj.pop()
-# param_3 = obj.top()
-# param_4 = obj.empty()
+        if root==None:
+            return None
+        l=Solution().invertTree(root.left)
+        r=Solution().invertTree(root.right)
+        root.left=r
+        root.right=l
+        return root
